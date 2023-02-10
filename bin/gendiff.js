21a7e708dd5c49d68a71f1ebc16673364cb5fba5
hdekjsne <hdekjsne@gmail.com>
2023-02-10 09:39:45 +0300

    add dependencies

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+export default function gendiff(path1, path2) {}
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
