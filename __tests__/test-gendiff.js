b532aab9a4a537d42c2b700e14f8e448b6818edc
hdekjsne <hdekjsne@gmail.com>
2023-02-12 16:21:18 +0300

    add tests, add gendiff

--- __tests__/test-gendiff.js
+++ __tests__/test-gendiff.js
@@ -1,5 +1,6 @@
 
-import { fullKeyListCostructor } from "../bin/gendiff.js"
+import { fullKeyListCostructor, makeArrLookLikeObj } from "../bin/gendiff.js";
+import gendiff from '../bin/gendiff.js';
 
 const file1 = {
   "host": "hexlet.io",
@@ -14,8 +15,33 @@ const file2 = {
   "host": "hexlet.io"
 }
 
+const example = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+
 test('check fullKeyListConstructor', () => {
   expect(fullKeyListCostructor(file1, file2)).toEqual(['follow', 'host', 'proxy', 'timeout', 'verbose']);
   expect(fullKeyListCostructor(file2, file1)).toEqual(['follow', 'host', 'proxy', 'timeout', 'verbose']);
   expect(fullKeyListCostructor([], [])).toEqual([]);
+});
+
+test('check makeArrLookLikeObj', () => {
+  const arr = [
+    ['aaaaaa', 'aaaaaaaaaaaa'],
+    ['question', 'a?'],
+    ['not a string', 2],
+    ['basket', ['eggs', 'milk', 'vinegar']]
+  ];
+  const str = `aaaaaa: aaaaaaaaaaaa\nquestion: a?\nnot a string: ${2}\nbasket: ${['eggs', 'milk', 'vinegar']}`;
+  expect(makeArrLookLikeObj(arr)).toEqual(str);
+  expect(makeArrLookLikeObj([])).toEqual('');
+});
+
+test('check gendiff', () => {
+  expect(gendiff('./file1.json', './file2.json')).toEqual(example);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { program } from 'commander';
 import * as fs from 'node:fs';
+import _ from 'lodash';
 
 export function fullKeyListCostructor(obj1, obj2) {
   const keys1 = Object.keys(obj1);
@@ -15,27 +16,41 @@ export function fullKeyListCostructor(obj1, obj2) {
   return commonKeysList.flat().sort();
 }
 
-export default function gendiff(path1, path2) {
-  const data1 = fs.readFileSync(path1, 'utf-8'); // string
-  const data2 = fs.readFileSync(path2, 'utf-8');
-  const parsedData1 = JSON.parse(data1); // object
-  const parsedData2 = JSON.parse(data2);
-  const keys = fullKeyListCostructor(parsedData1, parsedData2);
-  const result = {};
-  for (const key of keys) {
-    if (!Object.hasOwn(parsedData1, key)) {
-      result = Object.assign(result,);
-    }
-  }
+export function makeArrLookLikeObj(arrOfArrs) {
+  let copy = _.cloneDeep(arrOfArrs);
+  copy = copy.map((entrie) => entrie.join(': '));
+  copy = copy.join('\n');
+  return copy;
 }
 
-/*
-Ключ может:
-- появиться
-- исчезнуть
-- изменить значение
-[`  key`: value];
-*/
+export default function gendiff(path1, path2) {
+  const data1 = fs.readFileSync(path1, 'utf-8');
+  const data2 = fs.readFileSync(path2, 'utf-8');
+  const parsedData1 = JSON.parse(data1);
+  const parsedData2 = JSON.parse(data2);
+  const keys = fullKeyListCostructor(parsedData1, parsedData2);
+  const resultEntries = [];
+  for (const key of keys) {
+    if (!Object.hasOwn(parsedData1, key)) {
+      const strKey = `  + ${key}`;
+      resultEntries.push([strKey, parsedData2[key]]);
+    } else if (!Object.hasOwn(parsedData2, key)) {
+      const strKey = `  - ${key}`;
+      resultEntries.push([strKey, parsedData1[key]]);
+    } else if (!_.isEqual(parsedData1[key], parsedData2[key])) {
+      const strKey1 = `  - ${key}`;
+      const strKey2 = `  + ${key}`;
+      resultEntries.push([strKey1, parsedData1[key]]);
+      resultEntries.push([strKey2, parsedData2[key]]);
+    } else {
+      const strKey = `    ${key}`
+      resultEntries.push([strKey, parsedData1[key]]);
+    }
+  }
+  const almostResult = makeArrLookLikeObj(resultEntries);
+  const result = `{\n${almostResult}\n}`;
+  return result;
+}
 
 program
   .version('1.0.0')
