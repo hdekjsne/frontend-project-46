67c8f2460f45e4d2ddf0b2e8d235f2be64b5fd7a
hdekjsne <hdekjsne@gmail.com>
2023-02-14 05:10:19 +0300

    changes in main file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,15 @@ import { program } from 'commander';
 import gendiff from '../src/index.js';
 
 program
+  .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
+
+  .argument('<path1>', 'relative/absolute path to a first file')
+  .argument('<path2>', 'relative/absolute path to a second file')
+  .action((path1, path2) => {
+    console.log(gendiff(path1, path2))
+  })
 
 program.parse(process.argv)
