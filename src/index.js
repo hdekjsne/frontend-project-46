570bf82b12c7f7e037d015881ff671bbd11326de
na <hdekjsne@gmail.com>
2023-03-22 08:37:27 +0000

    .

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { makeStylish, makePlain, makeJson } from './formatter.js';
 export default function gendiff(path1, path2, type) {
   const data1 = parse(path1);
   const data2 = parse(path2);
+  /*
   switch(type) {
     case 'json':
       return JSON.stringify(makeJson(data1, data2), '', 2);
@@ -15,6 +16,14 @@ export default function gendiff(path1, path2, type) {
     default:
       return makeStylish(2, data1, data2);
   }
+  */
+  if (type === 'json') {
+   const obj = makeJson(data1, data2);
+   return JSON.stringify(obj, '', 2);
+  } else if (type === 'plain') {
+   return makePlain(data1, data2);
+  }
+  return makeStylish(2, data1, data2);
 }
 
  // console.log(gendiff('__fixtures__/file-recursive-1.json', '__fixtures__/file-recursive-2.json', 'json'));
